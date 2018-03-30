85558f2c2e463566fd45920ee388a4c5947d722c
Adraenwan <adraenwan@tfnux.org>
2018-03-30 17:09:59 +0200

    code cleanup

--- src/main/java/foo/projetglpoo/tableMaster/TableCreator.java
+++ src/main/java/foo/projetglpoo/tableMaster/TableCreator.java
@@ -1,133 +1,119 @@
 package foo.projetglpoo.tableMaster;
 
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
+import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
-
-import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.sql.Date;
 
-import javax.swing.AbstractAction;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
 public class TableCreator {
-	DefaultTableModel model;
-	JTable lotoTable;
-	JFrame Window;
+    private DefaultTableModel model;
+    private JTable lotoTable;
+    private JFrame Window;
 
-	JButton LaunchButton;
+    private JButton LaunchButton;
 
-	public void CreateTable(JFrame MainWindow, Object[][] tableDatas) {
-		Window = MainWindow;
+    public void CreateTable(JFrame MainWindow, Object[][] tableDatas) {
+        Window = MainWindow;
 
-		final String[] enTetes = { "Date", "1", "2", "3", "4", "5", "Star1", "Star2" };
-		SetTableModel(tableDatas, enTetes);
-		InitTableProp();
-		addTableButtons();
-	}
+        final String[] enTetes = {"Date", "1", "2", "3", "4", "5", "Star1", "Star2"};
+        SetTableModel(tableDatas, enTetes);
+        InitTableProp();
+        addTableButtons();
+    }
 
-	void addTableButtons() {
-		final JPanel ButtonPanel = new JPanel();
-		ButtonPanel.setBackground(Color.GRAY);
-		Window.getContentPane().add(ButtonPanel, BorderLayout.SOUTH);
+    void addTableButtons() {
+        final JPanel ButtonPanel = new JPanel();
+        ButtonPanel.setBackground(Color.GRAY);
+        Window.getContentPane().add(ButtonPanel, BorderLayout.SOUTH);
 
-		LaunchButton = new JButton(new LaunchGraphicGenAction());
-		ButtonPanel.add(LaunchButton);
-		
-		JButton datavisualB = new JButton(new LaunchDataVisual());
-		ButtonPanel.add(datavisualB);		
+        LaunchButton = new JButton(new LaunchGraphicGenAction());
+        ButtonPanel.add(LaunchButton);
 
-		LaunchButton.setEnabled(false);
-		datavisualB.setEnabled(true);
-	}
+        JButton datavisualB = new JButton(new LaunchDataVisual());
+        ButtonPanel.add(datavisualB);
 
-	void InitTableProp() {
-		lotoTable = new JTable(model);
+        LaunchButton.setEnabled(false);
+        datavisualB.setEnabled(true);
+    }
 
-		Window.getContentPane().add(new JScrollPane(lotoTable));
-		lotoTable.setAutoCreateRowSorter(true);
-		lotoTable.getSelectionModel().addListSelectionListener(new EventListener());	//add event listener to table so it can activate and deactivate buttons
-	}
+    void InitTableProp() {
+        lotoTable = new JTable(model);
 
-	void SetTableModel(Object[][] tableDatas, String[] enTetes) {	//model for column order
-		model = new DefaultTableModel(tableDatas, enTetes) {
-			@Override
-			public Class getColumnClass(int column) {
-				switch (column) {
-				case 0:
-					return Date.class;
-				default:
-					return int.class;
-				}
-			}
-		};
-	}
+        Window.getContentPane().add(new JScrollPane(lotoTable));
+        lotoTable.setAutoCreateRowSorter(true);
+        lotoTable.getSelectionModel().addListSelectionListener(new EventListener());    //add event listener to table so it can activate and deactivate buttons
+    }
 
-	private class LaunchGraphicGenAction extends AbstractAction {
-		public LaunchGraphicGenAction() {
-			super("Graphics generation");
-		}
+    void SetTableModel(Object[][] tableDatas, String[] enTetes) {    //model for column order
+        model = new DefaultTableModel(tableDatas, enTetes) {
+            @Override
+            public Class getColumnClass(int column) {
+                switch (column) {
+                    case 0:
+                        return Date.class;
+                    default:
+                        return int.class;
+                }
+            }
+        };
+    }
 
-		public void actionPerformed(ActionEvent arg0) {
-			JFrame drawframe = new JFrame("A new window for data drawing");
-			drawframe.setVisible(true);
+    Object[][] getSelectedColumnsFromTab() {    //get all the selected cells
+        int[] selected = lotoTable.getSelectedRows();
 
-			DrawDataSet(drawframe, getSelectedColumnsFromTab());
-		}
-	}
-	
-	private class LaunchDataVisual extends AbstractAction {
-		public LaunchDataVisual() {
-			super("View dataset apparition");
-		}
+        Object[][] tableToSend = new Object[selected.length][8];
 
-		public void actionPerformed(ActionEvent arg0) {
-			JFrame drawframe = new JFrame("A new window for data visual");
-			drawframe.setVisible(true);
-			
-			dataVisualisation(drawframe, getSelectedColumnsFromTab());
-		}
-	}
+        for (int i = 0; i < selected.length; i++)
+            for (int j = 0; j < 8; j++)
+                tableToSend[i][j] = model.getValueAt(selected[i], j);
 
-	private class EventListener implements ListSelectionListener {	//Button ativation event
-		public void valueChanged(ListSelectionEvent event) {
-			if (lotoTable.getSelectedRows().length > 0)
-			{
-				LaunchButton.setEnabled(true);
-			}
-			else
-			{
-				LaunchButton.setEnabled(false);
-			}
-		}
-	}
+        return tableToSend;
+    }
 
-	Object[][] getSelectedColumnsFromTab() {	//get all the selected cells
-		int[] selected = lotoTable.getSelectedRows();
+    void DrawDataSet(JFrame drawnWindow, Object[][] dataSet) {
+        // GRAPHIC PART TO LINK
+    }
 
-		Object[][] tableToSend = new Object[selected.length][8];
+    void dataVisualisation(JFrame drawnWindow, Object[][] dataSet) {
+        //TODO : data visualisation with JTable
+    }
 
-		for (int i = 0; i < selected.length; i++)
-			for (int j = 0; j < 8; j++)
-				tableToSend[i][j] = model.getValueAt(selected[i], j);
+    private class LaunchGraphicGenAction extends AbstractAction {
+        public LaunchGraphicGenAction() {
+            super("Graphics generation");
+        }
 
-		return tableToSend;
-	}
+        public void actionPerformed(ActionEvent arg0) {
+            JFrame drawframe = new JFrame("A new window for data drawing");
+            drawframe.setVisible(true);
 
-	void DrawDataSet(JFrame drawnWindow, Object[][] dataSet) {
-		// GRAPHIC PART TO LINK
-	}
-	
-	void dataVisualisation(JFrame drawnWindow, Object[][] dataSet)
-	{
-			//TODO : data visualisation with JTable
-		
-	}
+            DrawDataSet(drawframe, getSelectedColumnsFromTab());
+        }
+    }
 
+    private class LaunchDataVisual extends AbstractAction {
+        public LaunchDataVisual() {
+            super("View dataset apparition");
+        }
+
+        public void actionPerformed(ActionEvent arg0) {
+            JFrame drawframe = new JFrame("A new window for data visual");
+            drawframe.setVisible(true);
+
+            dataVisualisation(drawframe, getSelectedColumnsFromTab());
+        }
+    }
+
+    private class EventListener implements ListSelectionListener {    //Button ativation event
+        public void valueChanged(ListSelectionEvent event) {
+            if (lotoTable.getSelectedRows().length > 0) {
+                LaunchButton.setEnabled(true);
+            } else {
+                LaunchButton.setEnabled(false);
+            }
+        }
+    }
 }
--- src/main/java/foo/projetglpoo/tableMaster/WindowsOpener.java
+++ src/main/java/foo/projetglpoo/tableMaster/WindowsOpener.java
@@ -1,32 +1,26 @@
 package foo.projetglpoo.tableMaster;
 
-import java.awt.Dimension;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class WindowsOpener extends JFrame {
-	
-	JFrame MainWindow;
-	TableCreator dataTable;	
-	
-	
-	public WindowsOpener(Dimension WindowSize)
-	{
-		this.setSize(WindowSize);
-		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.setVisible(true);
-		this.setTitle("Euromillion drawer");
-		
-		MainWindow = this;
-	}
+    private JFrame MainWindow;
+    private TableCreator dataTable;
 
-    public void addTableToWindow( Object [][] dataSet )
-	{
-		dataTable = new TableCreator();
-		if(dataSet == null)
-		{
-			return;
-		}
-		dataTable.CreateTable( MainWindow , dataSet);
-	}
-	
+    public WindowsOpener(Dimension WindowSize) {
+        this.setSize(WindowSize);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setVisible(true);
+        this.setTitle("Euromillion drawer");
+
+        MainWindow = this;
+    }
+
+    public void addTableToWindow(Object[][] dataSet) {
+        dataTable = new TableCreator();
+        if (dataSet == null) {
+            return;
+        }
+        dataTable.CreateTable(MainWindow, dataSet);
+    }
 }
